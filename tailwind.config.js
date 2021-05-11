27008a8e5ba861516d444e947cdb3ee479a885a3
Bruno Lollato <brunopa.lollato@gmail.com>
2021-05-10 22:27:13 -0300

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
+}
